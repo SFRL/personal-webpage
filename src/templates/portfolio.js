8d5345969b4f85b18fdfec806e8b9ed5a79c7c65
SFRL <sebastian.lobbers@googlemail.com>
2021-08-21 17:12:12 +0100

    Fix component path

--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -2,7 +2,7 @@ import React from "react";
 import { graphql } from "gatsby";
 
 import Layout from "../components/layout";
-import Post from "../components/Post";
+import Post from "../components/post";
 import SEO from "../components/seo";
 
 import "../style/normalize.css";
